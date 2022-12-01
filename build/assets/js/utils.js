8121bd1c8b90faba1232e580eb700475011bf69b
Yasmin Medeiros <yasmin.medeirosapp@gmail.com>
2022-12-01 16:59:03 -0300

    build

---
+++ build/assets/js/utils.js
@@ -0,0 +1,16 @@
+let navToggle = document.querySelector(".nav-toggle");
+let navWrapper = document.querySelector(".nav-wrapper");
+
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close menu");
+    this.setAttribute("aria-expanded", "true");
+    searchForm.classList.remove("active");
+    searchToggle.classList.remove("active");
+  }
+});
